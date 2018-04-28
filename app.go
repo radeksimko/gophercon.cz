5388d64d1099729465798e5b1fc415c3fae53a3b
Radek Simko <radek.simko@gmail.com>
2018-04-28 21:22:32 +0100

    cleanup code and avoid redirects for robots.txt

--- app.go
+++ app.go
@@ -5,6 +5,16 @@ import (
 )
 
 func init() {
+	http.HandleFunc("/", redirectToGithub)
+	http.HandleFunc("/robots.txt", plainText)
+}
+
+func redirectToGithub(w http.ResponseWriter, r *http.Request) {
 	h := http.RedirectHandler("https://github.com/golang/go/wiki/Conferences", 302)
-	http.HandleFunc("/", h.ServeHTTP)
+	h.ServeHTTP(w, r)
+}
+
+func plainText(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(200)
 }
